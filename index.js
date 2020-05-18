1ffbdd59317fbb302b81e8745bea988bf6317db4
Dom Harrison <dom_harrison@live.co.uk>
2020-05-18 21:15:34 +0100

    voting functionality

--- index.js
+++ index.js
@@ -31,8 +31,6 @@ io.on('connection', (socket) => {
       room = roomName;
       user = {
         name,
-        role: '',
-        alive: true,
         id
       };
 
@@ -44,6 +42,11 @@ io.on('connection', (socket) => {
         rooms[room] = {
           name: room,
           dayCount: 0,
+          nightTime: false,
+          message: '',
+          votes: {},
+          voteCount: 0,
+          aliveCount: 0,
           hidden: {
             roles: [],
             actions: { mafia: {} },
@@ -54,7 +57,7 @@ io.on('connection', (socket) => {
         roomUsers[room] = [user];
       }
 
-      const roomStatus = { ...rooms[room], hidden: {} };
+      const roomStatus = { ...rooms[room] };
       io.to(room).emit('room_status', roomStatus);
       io.to(room).emit('room_users', roomUsers[room]);
     });
@@ -70,7 +73,7 @@ io.on('connection', (socket) => {
 
 
     socket.on('start_game', () => {
-      console.log('start_game in room ', room.name);
+      console.log('start_game in room', room);
       const gameRoles = shuffleArray(roles.slice(0, (roomUsers[room].length)));
       const mafiaUsers = []
 
@@ -79,7 +82,7 @@ io.on('connection', (socket) => {
         const socketId = roomUsers[room][ix].id;
         rooms[room].hidden.roles[ix] = [gameRole];
         if (gameRole !== 'villager') {
-          rooms[room].expectedActions ++
+          rooms[room].hidden.expectedActions ++
         } 
         if (gameRole !== 'mafia') {
           io.to(socketId).emit('room_users', [{ name: roomUsers[room][ix].name, role: gameRole }]);
@@ -96,49 +99,84 @@ io.on('connection', (socket) => {
         }
       });
 
+      rooms[room].aliveCount = roomUsers[room].length;
       rooms[room].dayCount = 1;
-      rooms[room].nightTime = true
-      const roomStatus = { ...rooms[room], hidden: {} };
+      rooms[room].nightTime = true;
+      const roomStatus = { ...rooms[room] };
       io.to(room).emit('room_status', roomStatus);
     });
 
 
-    socket.on('action', (target) => {
-      const actions = { ...rooms[room].hidden.actions };
-      rooms[room].actionCount++;
+    socket.on('action', ({ target, role }) => {
+      console.log('action:', user, target, role)
+      if (rooms[room].nightTime) {
+        const actions = { ...rooms[room].hidden.actions };
+        rooms[room].hidden.actionCount++;
 
-      if (user.role === 'mafia') {
-        actions.mafia = {
-          ...actions.mafia,
-          [target]: actions.mafia ? actions.mafia++ : 0
-        }
-      } else if (user.role === 'police') {
-        io.to(id).emit('private_message', `${target} is... `);
-      } else {
-        actions[user.role] = target;
-      }
-
-      if (rooms[room].actionCount === rooms[room].expectedActions) {
-        let killUser = undefined;
-        let message = ''
-        Object.keys(actions.mafia).forEach(trgt => {
-          if (!killUser || actions.mafia[trgt] > actions.mafia[killUser]) {
-            killUser = trgt;
-          }
-        })
-        if (actions.doctor && actions.doctor === killUser) {
-          rooms[room].message = `${killUser} was saved by the doctor`;
-          killUser = undefined;
+        if (role === 'mafia') {
+          actions.mafia[target] = actions.mafia[target] ? actions.mafia[target]++ : 1;
+        } else if (role === 'police') {
+          io.to(id).emit('private_message', `${target} is... `);
         } else {
-          rooms[room].message = `${killUser} was killed by the mafia`;
-          io.to(roomName).emit('room_users', [{ name: killUser, alive: false }]);
+          actions[role] = target;
+        }
+
+        rooms[room].hidden.actions = actions;
+        const roomStatus = { ...rooms[room] };
+        io.to(room).emit('room_status', roomStatus);
+  
+        if (rooms[room].hidden.actionCount === rooms[room].hidden.expectedActions) {
+          let killUser = undefined;
+          Object.keys(actions.mafia).forEach(trgt => {
+            if (!killUser || actions.mafia[trgt] > actions.mafia[killUser]) {
+              killUser = trgt;
+            }
+          })
+          if (actions.doctor && actions.doctor === killUser) {
+            rooms[room].message = `${killUser} was saved by the doctor`;
+            killUser = undefined;
+          } else {
+            const killUserIx = roomUsers[room].findIndex(user => user.name === killUser);
+            if (rooms[room].hidden.roles[killUserIx] !== 'villager') { rooms[room].hidden.expectedActions--; }
+            rooms[room].aliveCount--;
+            rooms[room].message = `${killUser} was killed by the mafia`;
+            io.to(room).emit('room_users', [{ name: killUser, dead: true }]);
+          }
+          
           rooms[room].dayCount++;
           rooms[room].nightTime = false;
+          rooms[room].hidden.actionCount = 0;
+          rooms[room].hidden.actions = { mafia: {} };
+        }
+  
+      } else {
+        rooms[room].votes[target] = rooms[room].votes[target] ? rooms[room].votes[target]++ : 1;
+        rooms[room].voteCount++;
+
+        const roomStatus = { ...rooms[room] };
+        io.to(room).emit('room_status', roomStatus);
+
+        if (rooms[room].voteCount === rooms[room].aliveCount) {
+          let killUser = undefined;
+          Object.keys(rooms[room].votes).forEach(trgt => {
+            if (!killUser || votes[trgt] > votes[killUser]) {
+              killUser = trgt;
+            }
+          });
+          
+          const killUserIx = roomUsers[room].findIndex(user => user.name === killUser);
+          if (rooms[room].hidden.roles[killUserIx] !== 'villager') { rooms[room].hidden.expectedActions--; }
+          rooms[room].aliveCount--;
+          rooms[room].message = `${killUser} was lynched by the village`;
+          io.to(room).emit('room_users', [{ name: killUser, dead: true }]);
+          
+          rooms[room].nightTime = true;
+          rooms[room].voteCount = 0;
+          rooms[room].votes = {};
         }
       }
 
-      rooms[room].hidden.actions = actions;
-      const roomStatus = { ...rooms[room], hidden: {} };
+      const roomStatus = { ...rooms[room] };
       io.to(room).emit('room_status', roomStatus);
     });
 
